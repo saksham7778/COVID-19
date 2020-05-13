ce9365d2618060afb9f972a76e1e6c23540502ee
saksam agarwal <saksamagr@gmail.com>
2020-05-13 21:23:47 +0530

    filtered api response

--- src/api/index.js
+++ src/api/index.js
@@ -7,9 +7,20 @@ export const fetchData = async () => {
 
     try {
         // selected only data part from api call
-        const response = await axios.get(url);
-
-        return response;
+        const { data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+        
+        const useful_data = {
+            // when names are same then we can directly give the values but for simplicity i have done by providing values
+            // confirmed,
+            // recovered,
+            // deaths,
+            // lastUpdate,
+            recovered:recovered,
+            confirmed:confirmed,
+            deaths:deaths,
+            lastUpdate:lastUpdate,
+        }
+        return useful_data;
     } catch (error) {
         
     }
